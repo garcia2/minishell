b075c0fc8db532f50e9be2d87c0231d604ec3351
Jim LEROUX <jileroux@e2r7p2.clusters.42paris.fr>
2023-03-01 16:02:22 +0100

    Mise en place du parsing

--- Makefile
+++ Makefile
@@ -1,5 +1,4 @@
-LEXER_FILES = main.c \
-			lexer.c \
+LEXER_FILES = lexer.c \
 			count_token.c \
 			is_white_space.c \
 			get_env.c \
@@ -7,9 +6,25 @@ LEXER_FILES = main.c \
 			skip.c \
 			spliters.c
 
+PARSER_FILES = parser.c\
+			init_utils.c\
+			init.c\
+			free.c
+
+SRCS_FILES = main.c
+
 LEXER = $(addprefix srcs/lexer/, $(LEXER_FILES))
 
+PARSER = $(addprefix srcs/parser/, $(PARSER_FILES))
+
+SRCS = $(addprefix srcs/, $(SRCS_FILES))
+
+
 LEXER_OBJS	= ${LEXER:.c=.o}
+
+PARSER_OBJS	= ${PARSER:.c=.o}
+
+SRCS_OBJS	= ${SRCS:.c=.o}
 
 NAME	= minishell
 
@@ -24,9 +39,9 @@ CFLAGS	= -Wall -Wextra -Werror -g3
 .c.o:
 		${CC} ${CFLAGS} -I includes/ -c $< -o ${<:.c=.o}
 
-${NAME}:	${LEXER_OBJS}
+${NAME}:	${LEXER_OBJS} ${PARSER_OBJS} ${SRCS_OBJS}
 		make -C libft
-		${CC} -o ${NAME} ${LEXER_OBJS} -I includes/ -L. ${LIBFT} -L/usr/lib/x86_64-linux-gnu  -lreadline
+		${CC} -o ${NAME} ${LEXER_OBJS} ${PARSER_OBJS} ${SRCS_OBJS} -I includes/ -L. ${LIBFT} -L/usr/lib/x86_64-linux-gnu  -lreadline
 
 all:		${NAME} bonus
 
@@ -35,10 +50,10 @@ clean:
 		${RM} ${LEXER_OBJS}
 
 fclean:		clean
-		make fclean -C libft 
+		make fclean -C libft
 		${RM} ${NAME}
 
 re:		clean all
 
 
-.PHONY:		all clean fclean re
+.PHONY:		all clean fclea
--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nicolas <nicolas@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jileroux <jileroux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 15:25:26 by nigarcia          #+#    #+#             */
-/*   Updated: 2023/02/17 14:24:43 by nicolas          ###   ########.fr       */
+/*   Updated: 2023/03/01 15:58:40 by jileroux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,46 @@
 # include <stdlib.h>
 # include <signal.h>
 
-int		is_white_space(char c);
-int		count_token(char *str);
-char	*get_env(char *str, int *i);
-char	*replace_env_var(char *str);
-void	go_next_quote(char	*str, int *i);
-void	go_next_double_quote(char	*str, int *i);
-void	skip_white_space(char *str, int *i);
-void	skip_token(char *str, int *i);
-char	*split_with_simple_quotes(char *str, int *i);
-char	*split_with_double_quotes(char *str, int *i);
-char	*split_without_quotes(char *str, int *i);
-char	**lexer(char *str);
-void	free_lexer(char **lex);
-void	print_lexer(char **lex);
+typedef struct s_cmd_table
+{
+	int					append;
+	char				*infile;
+	char				*here_doc;
+	char				*outfile;
+	char				**cmd;
+	struct s_cmd_table	*next;
+}	t_cmd_table;
+
+int			is_white_space(char c);
+int			count_token(char *str);
+char		*get_env(char *str, int *i);
+char		*replace_env_var(char *str);
+void		go_next_quote(char	*str, int *i);
+void		go_next_double_quote(char	*str, int *i);
+void		skip_white_space(char *str, int *i);
+void		skip_token(char *str, int *i);
+char		*split_with_simple_quotes(char *str, int *i);
+char		*split_with_double_quotes(char *str, int *i);
+char		*split_without_quotes(char *str, int *i);
+char		**lexer(char *str);
+void		free_lexer(char **lex);
+void		print_lexer(char **lex);
+
+/*******************************************************\
+|					PARSER FUNCTIONS					|
+\*******************************************************/
+
+t_cmd_table	*init_table(char **lex);
+t_cmd_table	*parser(char **lex);
+t_cmd_table	*init_node(void);
+void		init_redir(char **lex, t_cmd_table *cmd_table, int *i, int *j);
+int			cmd_size(char **lex);
+int			ft_strcmp(char *s1, char *s2);
+void		add_back(t_cmd_table **lst, t_cmd_table *new);
+void		init_tab(char **lex, t_cmd_table *cmd_table);
+int			is_delimiter(char *lex);
+void		init_cmd(char **lex, t_cmd_table *cmd_table);
+void		add_cmd(char **lex, char **cmd_table, int *i);
+void		clear_lst(t_cmd_table **lst);
 
 #endif
