ea83248989a72291db6e31849d974d4085f8d031
Jim LEROUX <jileroux@e3r12p1.clusters.42paris.fr>
2023-04-24 12:25:16 +0200

    mise en place signaux heredoc et correction du cd

--- srcs/exec/builtins/cd.c
+++ srcs/exec/builtins/cd.c
@@ -6,22 +6,43 @@
 /*   By: jileroux <jileroux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 12:11:56 by jileroux          #+#    #+#             */
-/*   Updated: 2023/03/17 13:56:17 by jileroux         ###   ########.fr       */
+/*   Updated: 2023/04/24 12:22:37 by jileroux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	change_directory(char *cmd)
+int	change_oldpwd(t_env_list *env)
+{
+	char		path[1024];
+	t_env_list	*env_tmp;
+
+	env_tmp = env;
+	while (env && ft_strcmp(env->key, "OLDPWD"))
+		env = env->next;
+	while (env_tmp && ft_strcmp(env_tmp->key, "PWD"))
+		env_tmp = env_tmp->next;
+	if (env->value)
+		free(env->value);
+	env->value = ft_strdup(env_tmp->value);
+	if (env_tmp->value)
+		free(env_tmp->value);
+	getcwd(path, 1024);
+	env_tmp->value = ft_strdup(path);
+	return (0);
+}
+
+int	change_directory(char *cmd, t_env_list *env)
 {
 	char	*path;
 
+	(void)env;
 	path = cmd;
-	printf("path : %s\n", path);
 	if (chdir(path) == -1)
 	{
 		printf("Minishell: cd: No such file or directory\n");
 		return (2);
 	}
+	change_oldpwd(env);
 	return (0);
 }
