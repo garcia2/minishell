8ba12f3f08e09d53cf03540e47908101636a749e
Nicolas GARCIA <nigarcia@e3r4p5.clusters.42paris.fr>
2023-04-26 16:08:13 +0200

    close in&out fd after dup

--- srcs/exec/dup_files.c
+++ srcs/exec/dup_files.c
@@ -6,7 +6,7 @@
 /*   By: nigarcia <nigarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 14:30:36 by nigarcia          #+#    #+#             */
-/*   Updated: 2023/04/05 17:52:13 by nigarcia         ###   ########.fr       */
+/*   Updated: 2023/04/26 16:05:42 by nigarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@ int	dup_files(t_cmd_table *cmd_table)
 	if (cmd_table->infile_fd > 2)
 	{
 		dup2(cmd_table->infile_fd, 0);
+		close(cmd_table->infile_fd);
 	}
 	if (cmd_table->outfile_fd > 2)
 	{
 		dup2(cmd_table->outfile_fd, 1);
+		close(cmd_table->outfile_fd);
 	}
 	return (1);
 }
